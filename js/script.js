c2084a951c647d51fb6007265754cb0a522d2fd7
Richard Zhang <geteralzx@gmail.com>
2020-02-09 19:37:25 -0600

    some cal

--- js/script.js
+++ js/script.js
@@ -1,23 +1,79 @@
-
-
-var n1;
-var n2;
+var a=0;
+var b=0;
 var key="";
 var input="";
+var checkNum="1234567890.";
+var operators="+-×÷=";
+var result=0;
+var opKey="";
 
-var numbers = document.querySelector(".calculator");
-var inputfield=document.querySelector(".input")
+var keys = document.querySelector(".calculator");
+var inputField=document.querySelector(".input");
 
-numbers.addEventListener('click',function(e){
-    // console.log(e.target.innerText.toString());
+
+let calc=function(a,b){
+        switch(opKey){
+            case "+":
+                return a + b;
+                break;
+            case "-":
+                return b - a;
+                break;
+            case "×":
+                return a * b;
+                break;
+            case "÷":
+                return b / a;
+                break;
+            case "=":
+                break;
+        }
+    }
+
+keys.addEventListener('click',function(e){
+
     key = e.target.innerText;
-    if (key!="+" && key!="+"&& key!="-"&& key!="&divide;"&& key!="&times;"&& key!="=" && key!="C"){
-        input=input + key;
-    }
-    if{
-        input="operator";
-    }
-    console.log(input);
-})
+    if (checkNum.includes(key)){
+        if(inputField.innerText.includes(".")&&key=="."){}//ignore duplicate "."
 
-inputfield.append(key);
+        else{
+        inputField.innerText=inputField.innerText + key;
+        a=parseFloat(inputField.innerText);  console.log("a is "+a.toString());    
+        }
+    }
+
+    else if (operators.includes(key)){
+    //1,check previous operator: call operation function
+        // highlight the button
+    //2,store inputField into value a then,
+    //3,set addition function ready when next operator click
+
+
+        if(opKey!=""){
+            
+
+            result=calc(a,b);
+            console.log(result);
+            // inputField.innerText=result;
+            // opKey="";
+        }
+        opKey=key;
+        console.log("operation:" + opKey);
+        
+        b=a;        
+        console.log("b is "+b);
+
+        // inputField.innerText="";
+        inputField.innerText=result;
+        a=result;
+        console.log("a is "+a.toString()); 
+    }
+
+    else if(e.target.innerText == "C"){
+        inputField.innerText = "";
+        a=0;
+        b=0;
+        result=0;
+        opKey='';
+    }
+})
--- test.js
+++ test.js
@@ -12,3 +12,53 @@ function acronym(arr) {
 
 
 // console.log(acronym(['very', 'important', 'person']));
+
+
+let buttons = document.querySelector(".buttons");
+let result = document.getElementById("result");
+let input = document.getElementById('input');
+let numStorage = [];
+let stringStorage = [];
+let sum = 0;
+let limitOperation = false;
+let validChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+let validOpChar = ["+"];
+buttons.addEventListener('click', function(e){
+    // console.log(e.target.textContent);
+    //if e.target is not an equal sign or a clear sign then append a string input
+    // when e.target is the equal sign, then split the string by the + symbol
+    // add all of the content using the reduce method 
+    // display results on our screen
+    // if C is entered, reset all of our arrays values
+    if(validChar.includes(e.target.textContent)  && limitOperation == false){
+        console.log("found");
+        input.textContent = input.textContent + e.target.textContent;
+    }
+    else if (validOpChar.includes(e.target.textContent)){
+        input.textContent = input.textContent + e.target.textContent;
+        limitOperation = false;
+    }
+    else if (e.target.textContent == "="){
+        stringStorage = input.textContent.split("+");
+        console.log(stringStorage);
+        let numStorage = stringStorage.map(function(strNum){
+            return parseFloat(strNum);
+        })
+        console.log(numStorage);
+        sum = numStorage.reduce(function(x, y){
+            return x + y
+        })
+        console.log(sum);
+        input.textContent = sum.toString();
+        numStorage = [];
+        stringStorage = [];
+        limitOperation = true;
+    }
+    else if(e.target.textContent == "C"){
+        input.textContent = "";
+        numStorage = [];
+        stringStorage = [];
+        limitOperation = false;
+        sum = 0;
+    }
+})
